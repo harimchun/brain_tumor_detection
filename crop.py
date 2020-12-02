165d2952620201c5efc2c6058bedf8f0359ac817
harimchun <61618700+harimchun@users.noreply.github.com>
2020-12-03 04:17:36 +0900

    Initial commit

---
+++ crop.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+import os
+from os import listdir
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+import cv2
+import matplotlib.pyplot as plt
+import imutils
+
+
+def crop_brain_contour(image, plot=False):
+    # Convert the image to grayscale, and blur it slightly
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.erode(thresh, None, iterations=2)
+
+
+    # Find contours in thresholded image, then grab the largest one
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    c = max(cnts, key=cv2.contourArea)
+    # extreme points
+    extLeft = tuple(c[c[:, :, 0].argmin()][0])
+    extRight = tuple(c[c[:, :, 0].argmax()][0])
+    extTop = tuple(c[c[:, :, 1].argmin()][0])
+    extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+    # crop new image out of the original image using the four extreme points (left, right, top, bottom)
+    new_image = image[extTop[1]:extBot[1], extLeft[0]:extRight[0]]
+
+    if plot:
+        plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(image)
+    plt.tick_params(axis='both', which='both', top=False, bottom=False, left=False, right=False, labelbottom=False,
+                    labeltop=False, labelleft=False, labelright=False)
+    plt.title('Original Image')
+    plt.subplot(1, 2, 2)
+    plt.imshow(new_image)
+    plt.tick_params(axis='both', which='both', top=False, bottom=False, left=False, right=False, labelbottom=False,
+                    labeltop=False, labelleft=False, labelright=False)
+    plt.title('Cropped Image')
+    plt.show()
+
+    return new_image
---
+++ main.py
@@ -0,0 +1,73 @@
+import flask
+from flask import Flask, request, render_template
+import joblib
+from tensorflow.keras.models import Model,load_model
+import numpy as np
+from scipy import misc
+import cv2
+import crop
+import matplotlib.pyplot as plt
+import time
+
+best_model = load_model(filepath = "/Users/harim/Documents/GitHub/brain_tumor_detection/cnn-parameters-improvement-25-0.91.model")
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+  return flask.render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def make_prediction():
+  if request.method == 'POST':
+
+    file = request.files['image']
+    if not file:
+      return render_template("index.html", label="No Files")
+
+    # reading image
+    prediction = []
+
+    IMG_WIDTH, IMG_HEIGHT = (240, 240)
+
+    img = cv2.imdecode(np.fromstring(file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+    ex_crop_img = crop.crop_brain_contour(img, plot=False)
+    ex_crop_img = cv2.resize(ex_crop_img, dsize=(IMG_WIDTH, IMG_HEIGHT), interpolation=cv2.INTER_CUBIC)
+    # normalize values
+    ex_crop_img = ex_crop_img / 255.
+
+    prediction.append(ex_crop_img)
+    prediction = np.array(prediction)
+
+    score = round(float(best_model.predict(prediction)),3)
+
+    result = ''
+
+    if score> 0.4:
+      result = "Tumor가 발견되었습니다"
+    else:
+      result = "Tumor가 발견되지 않았습니다"
+
+    label = "Score:"+str(score)+"\n"+result
+
+    plt.figure()
+    plt.subplot(1, 2, 1)
+    plt.imshow(img)
+    plt.tick_params(axis='both', which='both', top=False, bottom=False, left=False, right=False, labelbottom=False,
+                    labeltop=False, labelleft=False, labelright=False)
+    plt.title('Original Image')
+    plt.subplot(1, 2, 2)
+    plt.imshow(ex_crop_img)
+    plt.tick_params(axis='both', which='both', top=False, bottom=False, left=False, right=False, labelbottom=False,
+                    labeltop=False, labelleft=False, labelright=False)
+    plt.title('Cropped Image')
+
+    img_name = time.strftime('%H%M%S')+".png"
+
+    plt.savefig("static/"+img_name)
+    time.sleep(0.001)
+    return render_template("index.html", label=label, img = img_name)
+
+if __name__=='__main__':
+  app.run(host='0.0.0.0', port=8888, debug=True)
